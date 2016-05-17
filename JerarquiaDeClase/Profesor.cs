109ac3f9cfdc42eba0174c6c3c1c3c91d2448a75
unknown <alumno@LAB02PC16.labs.extension>
2016-05-17 11:44:24 -0300

    Jerarquia

---
+++ JerarquiaDeClase/Profesor.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JerarquiaDeClase
+{
+    class Profesor:Persona
+    {
+
+       
+
+
+        public override string Nombre
+        {
+            get
+            {
+                return this._nombre;
+            }
+            set
+            {
+               // throw new NotImplementedException();
+                this._nombre = value;
+            }
+        }
+
+        public override void mostrar()
+        {
+            Console.WriteLine(this._nombre);// throw new NotImplementedException();
+        }
+
+        public override void algovirtual()
+        {
+            base.algovirtual();
+        }
+    }
+}
---
+++ JerarquiaDeClase/SerHumano.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JerarquiaDeClase
+{
+    public abstract class SerHumano
+    {
+        public string FechaNacimiento;
+        public abstract void mostrarFecha();
+
+        public virtual void algovirtual()
+        { 
+
+        }
+        
+
+    }
+}
